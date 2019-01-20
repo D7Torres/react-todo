b18660fe0e53b5580d2a5d8d45ec7d3e9a0ee884
David Torres <david7torres@gmail.com>
2019-01-20 23:12:26 +0000

    Add sliders to toDo items for importance and urgency

--- src/uicomponents/ToDoItem/ToDoItem.presentation.js
+++ src/uicomponents/ToDoItem/ToDoItem.presentation.js
@@ -1,8 +1,19 @@
 import React, { Fragment } from 'react'
-import css from './ToDoItem.module.css'
 import classnames from 'classnames'
+import Slider from '@material-ui/lab/Slider';
+import css from './ToDoItem.module.css'
 
-const ToDoItemPresentation = ({ id, label, isDone, onChange, deleteTodo }) => {
+const ToDoItemPresentation = ({
+    id,
+    label,
+    isDone,
+    importance,
+    importanceOnChange,
+    urgency,
+    urgencyOnChange,
+    onChange,
+    deleteTodo,
+}) => {
     const labelClass = classnames({ [css.isDone]: isDone })
 
     return (
@@ -10,6 +21,22 @@ const ToDoItemPresentation = ({ id, label, isDone, onChange, deleteTodo }) => {
             <input id={id} type="checkbox" checked={isDone} onChange={onChange} />
             <span className={labelClass}>{label}</span>
             <i onClick={(event) => deleteTodo(event.target.parentNode.querySelector('input').id)}>Borrar</i>
+            Importance:
+            <Slider
+                value={importance}
+                min={0}
+                max={5}
+                step={1}
+                onChange={importanceOnChange}
+            />
+            Urgency:
+            <Slider
+                value={urgency}
+                min={0}
+                max={5}
+                step={1}
+                onChange={urgencyOnChange}
+            />
         </Fragment>
     )
 }
