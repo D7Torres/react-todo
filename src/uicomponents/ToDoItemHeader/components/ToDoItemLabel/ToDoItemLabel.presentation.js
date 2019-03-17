8c0821bc82548254c459e8fba24cb973eec0f839
David Torres <david7torres@gmail.com>
2019-03-17 19:38:30 +0000

    Style the cursor when hovering the label

--- src/uicomponents/ToDoItemHeader/components/ToDoItemLabel/ToDoItemLabel.presentation.js
+++ src/uicomponents/ToDoItemHeader/components/ToDoItemLabel/ToDoItemLabel.presentation.js
@@ -24,7 +24,7 @@ const ToDoItemLabelPresentation = ({
       />
     );
   }
-  const labelClass = classnames({ [css.crossed]: isCrossed });
+  const labelClass = classnames(css.label, { [css.crossed]: isCrossed });
 
   return (
     <label className={labelClass} onClick={() => setIsEditEnabled(true)}>
